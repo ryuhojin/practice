9100eef5092155b056f3836060836d599044d217
ryuhojin <interarea@naver.com>
2023-04-06 12:54:24 +0900

    개발 : 정규식 빌드 부분 간소화 및 직관적으로 변경

--- libs/regEasy/__tests__/index.test.ts
+++ libs/regEasy/__tests__/index.test.ts
@@ -1,45 +1,8 @@
 // __tests__/index.test.ts
-import RegEasy from "../src/index";
+import { RegEasy, RegOpts } from "../src/index";
 
 test("Build Regex Phone Number", () => {
-  expect(new RegEasy().start().char("123123").end().build().toRegex() + "").toBe("/^123123$/"
-  );
-});
-
-test("Test Regex Phone Number", () => {
-  const regex = new RegEasy()
-    .start()
-    .char("01")
-    .oneOf(["1", "7", "6", "9", "0"])
-    .char("-")
-    .number()
-    .between(3, 4)
-    .char("-")
-    .number()
-    .between(4, 4)
-    .build();
-  expect(regex.test("010-341-1234")).toBe(true);
-});
-
-test("Test Regex Email Address", () => {
-  const regex = new RegEasy()
-    .both()
-    .more(1)
-    .char('@')
-    .both()
-    .more(1)
-    .char('.')
-    .text()
-    .more(1)
-    .build();
-
-  expect(regex.test("kik3926@naver.com")).toBe(true);
-});
-
-
-test("Test Regex Email Address", () => {
-  const reg = new RegEasy()
-  .text().build();
-  
-  expect(reg.replace("","")).toBe("");
+  expect(
+    new RegEasy().both(RegOpts.eng, RegOpts.kor, RegOpts._).toRegex() + ""
+  ).toBe("/[a-zA-Z가-힣_]/");
 });
---
+++ libs/regEasy/src/RegOpts.ts
@@ -0,0 +1,6 @@
+export const RegOpts = {
+  eng: "a-zA-Z",
+  kor: "가-힣",
+  num: "0-9",
+  _: "_",
+};
