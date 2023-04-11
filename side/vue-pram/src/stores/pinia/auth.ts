040b43894f589c3eedf310b9922f4de49cd1af26
ryuhojin <interarea@naver.com>
2023-04-11 09:02:28 +0900

    업데이트

---
+++ side/vue-pram/src/stores/pinia/auth.ts
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+import { reactive, computed } from "vue";
+
+interface userProps {
+  NAME?: string;
+  ROLE?: Array<string>;
+}
+
+export const useAuth = defineStore("auth", () => {
+  const user: userProps = reactive({});
+
+  const getNAME = computed(() => user.NAME);
+  const getROLE = computed(() => user.ROLE);
+  return { user, getNAME, getROLE };
+});
